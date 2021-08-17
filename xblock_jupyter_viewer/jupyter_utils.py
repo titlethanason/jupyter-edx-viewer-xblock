8dbdcd6343ed1fde4e3cbd8fd8822743706a6418
titlethanason <titlethanason@gmail.com>
2021-08-17 17:23:19 +0700

    :arrow_up: (Tutor integration) move preprocesss.py and postprocess.py to util

--- xblock_jupyter_viewer/jupyter_utils.py
+++ xblock_jupyter_viewer/jupyter_utils.py
@@ -6,11 +6,83 @@ import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
 from nbconvert import HTMLExporter
 
-from preprocessors import ImageReplacement, RemoveCustomCSS
-from post_processors import remove_box_shadow, insert_target_blank
+# from preprocessors import ImageReplacement, RemoveCustomCSS
+# from post_processors import remove_box_shadow, insert_target_blank
 
+import re
 
 log = logging.getLogger(__name__)
+
+# preprocess
+class Processor(object):
+    """Base cell transformer - applies `process_cell` to each cell"""
+
+    def __init__(self, nb):
+        self.nb = nb
+
+    def process_cell(self, cell):
+        raise NotImplemented
+
+    def finish(self):
+        """Optional function to be called after iterations are complete"""
+        pass
+
+
+class RemoveCustomCSS(Processor):
+    """Remove the cell that loads custom css if it's present"""
+
+    def __init__(self, nb):
+        super(RemoveCustomCSS, self).__init__(nb)
+        self.search_text = 'from IPython.core.display import HTML'
+        self.found = False
+        self.cell_num = 0
+
+    def process_cell(self, cell):
+        if not self.found:
+            if self.search_text in cell['source']:
+                log.debug("Found Custom CSS Cell @ cells[{}]".format(self.cell_num))
+                self.found = True
+                return
+            self.cell_num += 1
+
+    def finish(self):
+        """Remove custom css cell if found"""
+        if self.found:
+            del self.nb['cells'][self.cell_num]
+            log.debug("Removed cell #: {} [custom css]".format(self.cell_num))
+
+
+class ImageReplacement(Processor):
+    """Replaces img src attribute with absolute path"""
+
+    def __init__(self, nb, images_url):
+        super(ImageReplacement, self).__init__(nb)
+        self.images_url = images_url
+
+    def process_cell(self, cell):
+        matches = re.findall(r'<img.+src=\"(.+?)\"', cell['source'])
+        for m in matches:
+            tail = m.split('/')[-1]
+            cell['source'] = cell['source'].replace(m, '{}{}'.format(self.images_url, tail))
+
+#post process
+def remove_box_shadow(raw_html):
+    """Removes box shadow around document by replacing defining class name"""
+    container = "#notebook-container"
+    log.debug("Found #notebook-container class: {}".format(raw_html.find(container)))
+
+    return raw_html.replace(container, "#doesnotexisthere")
+
+
+def insert_target_blank(raw_html):
+    """Adds 'target=_blank' attribute to all `<a href=...>` links """
+    return re.sub('(<a .+?>)', _match_fn, raw_html.encode('utf-8'))
+
+
+def _match_fn(matchobj):
+    """Return original <a href...> with `target='_blank'` inserted"""
+    s = matchobj.group(0)
+    return '{} target="_blank" {}'.format(s[:2], s[3:])
 
 
 def fetch_notebook(url):
