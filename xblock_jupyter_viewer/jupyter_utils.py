3c35123e72efc88c6d8524ed879ad8e86ffb68a6
titlethanason <titlethanason@gmail.com>
2021-08-18 14:32:02 +0700

    :arrow_up: (Tutor integration) try to fix error by change .decode('utf-8') in re.sub

--- xblock_jupyter_viewer/jupyter_utils.py
+++ xblock_jupyter_viewer/jupyter_utils.py
@@ -76,7 +76,7 @@ def remove_box_shadow(raw_html):
 
 def insert_target_blank(raw_html):
     """Adds 'target=_blank' attribute to all `<a href=...>` links """
-    return re.sub('(<a .+?>)', _match_fn, raw_html.encode('utf-8'))
+    return re.sub('(<a .+?>)', _match_fn, raw_html)
 
 
 def _match_fn(matchobj):
